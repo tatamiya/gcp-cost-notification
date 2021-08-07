8556e08baaf2bacdcb672f3240d3ea13bc0ad4f0
tatamiya <tamyuji1002@gmail.com>
2021-08-07 20:37:01 +0900

    add integration tests

--- main_test.go
+++ main_test.go
@@ -33,5 +33,55 @@ BigQuery: ¥ 0.07 (¥ 0)`
 
 	assert.Nil(t, err)
 	assert.EqualValues(t, expectedMessage, actualMessage)
+}
 
+func TestWithNoServiceCosts(t *testing.T) {
+
+	reportingDateTime := time.Date(2021, 8, 7, 20, 15, 0, 0, time.Local)
+	inputQueryResults := []*db.QueryResult{
+		{Service: "Total", Monthly: 1000.07, Yesterday: 400.0},
+	}
+	BQClientStub := db.NewBQClientStub(inputQueryResults, nil)
+	SlackClientStub := notification.NewSlackClientStub(nil)
+
+	expectedMessage :=
+		"＜8/1 ~ 8/6 の GCP 利用料金＞ ※ () 内は前日分\n\nTotal: ¥ 1,000.07 (¥ 400)"
+
+	actualMessage, err := mainProcess(reportingDateTime, &BQClientStub, &SlackClientStub)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedMessage, actualMessage)
+}
+
+func TestWithEmptyQueryResult(t *testing.T) {
+
+	reportingDateTime := time.Date(2021, 8, 7, 20, 15, 0, 0, time.Local)
+	inputQueryResults := []*db.QueryResult{}
+	BQClientStub := db.NewBQClientStub(inputQueryResults, nil)
+	SlackClientStub := notification.NewSlackClientStub(nil)
+
+	expectedMessage :=
+		"＜8/1 ~ 8/6 の GCP 利用料金＞ ※ () 内は前日分\n\nTotal: ¥ 0 (¥ 0)"
+
+	actualMessage, err := mainProcess(reportingDateTime, &BQClientStub, &SlackClientStub)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedMessage, actualMessage)
+}
+
+func TestWithUnsortedQueryResult(t *testing.T) {
+
+	reportingDateTime := time.Date(2021, 8, 7, 20, 15, 0, 0, time.Local)
+	inputQueryResults := []*db.QueryResult{
+		{Service: "Cloud SQL", Monthly: 1000.0, Yesterday: 400.0},
+		{Service: "BigQuery", Monthly: 0.07, Yesterday: 0.0},
+		{Service: "Total", Monthly: 1000.07, Yesterday: 400.0},
+	}
+	BQClientStub := db.NewBQClientStub(inputQueryResults, nil)
+	SlackClientStub := notification.NewSlackClientStub(nil)
+
+	actualMessage, err := mainProcess(reportingDateTime, &BQClientStub, &SlackClientStub)
+
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "", actualMessage)
 }
