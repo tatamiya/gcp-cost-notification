3f353fda3d03165021c7e95876156b34cd508345
tatamiya <tamyuji1002@gmail.com>
2021-08-07 20:22:48 +0900

    add integration test using client stub

--- main_test.go
+++ main_test.go
@@ -1,16 +1,25 @@
 package gcp_cost_notification
 
 import (
-	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tatamiya/gcp-cost-notification/db"
+	"github.com/tatamiya/gcp-cost-notification/notification"
 )
 
-func TestCostNotifierAll(t *testing.T) {
-	m := PubSubMessage{
-		Data: []byte(""),
+func TestRunWholeProcessCorrectly(t *testing.T) {
+
+	reportingDateTime := time.Date(2021, 8, 7, 20, 15, 0, 0, time.Local)
+	inputQueryResults := []*db.QueryResult{
+		{Service: "Total", Monthly: 1000.07, Yesterday: 400.0},
+		{Service: "Cloud SQL", Monthly: 1000.0, Yesterday: 400.0},
+		{Service: "BigQuery", Monthly: 0.07, Yesterday: 0.0},
 	}
-	err := CostNotifier(context.Background(), m)
+	BQClientStub := db.NewBQClientStub(inputQueryResults, nil)
+	SlackClientStub := notification.NewSlackClientStub(nil)
+
+	err := mainProcess(reportingDateTime, &BQClientStub, &SlackClientStub)
 	assert.Nil(t, err)
 }
