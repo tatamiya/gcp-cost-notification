341737bb9160494cda51f6327e9df1ac3f75b886
tatamiya <tamyuji1002@gmail.com>
2021-08-07 15:14:18 +0900

    add BQ client stub

---
+++ db/client_stub.go
@@ -0,0 +1,16 @@
+package db
+
+type BQClientStub struct {
+	records []*QueryResult
+	err     error
+}
+
+func NewBQClientStub(results []*QueryResult, err error) BQClientStub {
+	return BQClientStub{
+		records: results,
+		err:     err,
+	}
+}
+func (c *BQClientStub) SendQuery(query string) ([]*QueryResult, error) {
+	return c.records, c.err
+}
