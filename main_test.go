cf7e51e160b33bd98753bb636efc3e5d3ac1ad1d
tatamiya <tamyuji1002@gmail.com>
2021-08-08 12:18:33 +0900

    add integration test for excuting on the first day of month

--- main_test.go
+++ main_test.go
@@ -37,6 +37,32 @@ BigQuery: ¥ 0.07 (¥ 0)`
 	assert.EqualValues(t, expectedMessage, actualMessage)
 }
 
+func TestDisplayPreviousMonthConstsOnFirstDayOfMonth(t *testing.T) {
+
+	reportingDateTime := time.Date(2021, 8, 1, 0, 0, 0, 0, time.Local)
+	inputQueryResults := []*db.QueryResult{
+		{Service: "Total", Monthly: 1000.07, Yesterday: 400.0},
+		{Service: "Cloud SQL", Monthly: 1000.0, Yesterday: 400.0},
+		{Service: "BigQuery", Monthly: 0.07, Yesterday: 0.0},
+	}
+	BQClientStub := db.NewBQClientStub(inputQueryResults, nil)
+	SlackClientStub := notification.NewSlackClientStub(nil)
+
+	expectedMessage :=
+		`＜7/1 ~ 7/31 の GCP 利用料金＞ ※ () 内は前日分
+
+Total: ¥ 1,000.07 (¥ 400)
+
+----- 内訳 -----
+Cloud SQL: ¥ 1,000 (¥ 400)
+BigQuery: ¥ 0.07 (¥ 0)`
+
+	actualMessage, err := mainProcess(reportingDateTime, &BQClientStub, &SlackClientStub)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, expectedMessage, actualMessage)
+}
+
 func TestWithNoServiceCosts(t *testing.T) {
 
 	reportingDateTime := time.Date(2021, 8, 7, 20, 15, 0, 0, time.Local)
