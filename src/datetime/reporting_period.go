8528a7d57a02753652dcfad4055d89c9177a243a
tatamiya <tamyuji1002@gmail.com>
2021-08-30 23:09:52 +0900

    add docs to reporting period object and function

--- src/datetime/reporting_period.go
+++ src/datetime/reporting_period.go
@@ -4,12 +4,23 @@ package datetime
 
 import "time"
 
+// ReportingPeriod contains the date period
+// to aggregate and report the GCP cost.
 type ReportingPeriod struct {
 	TimeZone string
 	From     time.Time
 	To       time.Time
 }
 
+// NewReportingPeriod constructs the date period to report the GCP cost
+// from the reporting datetime.
+//
+// The period is from the first day of the month to the one day before
+// the reporting date. (e.g. 2021/8/30 -> 2021/8/1 ~ 2021/8/29)
+//
+// If the reporting date is the first day of a month,
+// the period starts from the first day of the previous month.
+// (e.g. 2021/8/1 -> 2021/7/1 ~ 2021/7/31)
 func NewReportingPeriod(reportingDateTime time.Time) ReportingPeriod {
 	location := reportingDateTime.Location()
 	oneDayBefore := reportingDateTime.AddDate(0, 0, -1)
