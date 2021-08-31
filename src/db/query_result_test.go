322dcfcc83765b4ed0fbe0e01fe79bfcf4c92f7c
tatamiya <tamyuji1002@gmail.com>
2021-08-31 23:27:48 +0900

    clean: convert a test into an example

--- src/db/query_result_test.go
+++ src/db/query_result_test.go
@@ -1,18 +1,13 @@
 package db
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
-func TestCorrectlyDisplayStringOfQueryResult(t *testing.T) {
+func ExampleQueryResult_String() {
 	sampleQueryResult := &QueryResult{
 		Service: "Cloud SQL", Monthly: 1000.0, Yesterday: 400.0,
 	}
-	expectedString := "{Service: Cloud SQL, Monthly: 1000.000000, Yesterday: 400.000000}"
-	actualString := sampleQueryResult.String()
-
-	assert.EqualValues(t, expectedString, actualString)
-
+	fmt.Println(sampleQueryResult.String())
+	// Output: {Service: Cloud SQL, Monthly: 1000.000000, Yesterday: 400.000000}
 }
