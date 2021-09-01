953af81c06f4d907053fee34b4689a4eb31184d4
tatamiya <tamyuji1002@gmail.com>
2021-09-01 23:30:45 +0900

    add docs to notification package

--- src/notification/slack_client.go
+++ src/notification/slack_client.go
@@ -1,3 +1,4 @@
+// notification package implements an object to send a message to Slack
 package notification
 
 import (
@@ -15,19 +16,28 @@ func NewSlackError(message string, err error) *utils.CustomError {
 	}
 }
 
+// An object implemented with Messenger interface
+// can be converted into a notification message to send to Slack.
 type Messenger interface {
 	AsMessage() string
 }
 
+// SlackClient is an object to send a message to Slack
+// via webhook URL.
 type SlackClient struct {
 	webhookURL string
 }
 
+// NewSlackClient constructs a SlackClient object.
+// The webhook URL is fetched from the environment variable
+// `SLACK_WEBHOOK_URL` in construction.
 func NewSlackClient() SlackClient {
 	webhookURL := os.Getenv("SLACK_WEBHOOK_URL")
 	return SlackClient{webhookURL: webhookURL}
 }
 
+// Send method receives an object which can be converted into
+// a notification message and sends it to Slack.
 func (c *SlackClient) Send(messenger Messenger) (string, *utils.CustomError) {
 	message := messenger.AsMessage()
 	msg := slack.WebhookMessage{
