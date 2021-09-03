b940eee8a9b80bfa044d549468de9c0a0bb0482e
tatamiya <tamyuji1002@gmail.com>
2021-09-03 21:17:34 +0900

    add docs to utils package

--- src/utils/error.go
+++ src/utils/error.go
@@ -1,16 +1,20 @@
+// utils package implements objects commonly used in other packages.
 package utils
 
 import "fmt"
 
+// CustomError wraps an error with additional information
 type CustomError struct {
-	Process string
-	Message string
-	Err     error
+	Process string // In which process the error took place
+	Message string // Detail of the error
+	Err     error  // Wrapping an original error
 }
 
 func (e *CustomError) Error() string {
 	return fmt.Sprintf("Error in %s. %s: %s", e.Process, e.Message, e.Err)
 }
+
+// AsMessage method creates a message to notify the error to Slack
 func (e *CustomError) AsMessage() string {
 	return fmt.Sprintf("Process Failed at %s!", e.Process)
 }
